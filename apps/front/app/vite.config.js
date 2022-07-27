c58aa7f01a39e43d56baa3e7857406bd7ebc64f3
Simone Baffelli <simone.baffelli@empa.ch>
2022-07-27 15:52:29 +0200

    Added vite configuration.

--- apps/front/app/vite.config.js
+++ apps/front/app/vite.config.js
@@ -11,12 +11,19 @@ export default defineConfig({
         },
     },
     server: {
+        host: true,
+        port: 5173,
         proxy: {
         '/openbis' : {
             ws: true,
             changeOrigin: true,
             secure: false,
-            target: "https://localhost:8443/openbis/openbis/"}
+            target: "https://openbis:443/openbis/openbis/"},
+        '/data-discovery' : {
+            ws: true,
+            changeOrigin: true,
+            secure: false,
+            target: "https://data-discovery:80/"}
         }
     }
 });
