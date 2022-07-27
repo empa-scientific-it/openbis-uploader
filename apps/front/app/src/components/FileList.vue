a13fda3de7c15c6016d5e1ef1181ee249b269a29
Simone Baffelli <simone.baffelli@empa.ch>
2022-07-27 15:53:53 +0200

    Added vue component to show a list of files.

---
+++ apps/front/app/src/components/FileList.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul id="example-1">
+    <li v-for="item in items" :key="item.message">
+        {{ item.message }}
+    </li>
+    </ul>
+</template>
+
+
+<script setup lang="ts">
+    import { useUser } from '@/stores/login'
+    import { useFile } from '@/stores/files'
+    const auth = useUser();
+    const user = auth.user;
+</script>
