7ae891ee4b464d7fb4f3472646481e67d26be01e
Simone Baffelli <simone.baffelli@empa.ch>
2022-09-08 16:23:35 +0200

    Added faster method to represent the openbis tree

--- services/openbis-configuration/instance_creator/instance_creator/views.py
+++ services/openbis-configuration/instance_creator/instance_creator/views.py
@@ -1,13 +1,30 @@
 
+from ast import Str
+from re import S
 import pybis
 from typing import List, Generator, Dict, Optional, Any
 
 from pydantic import BaseModel, fields
 from dataclasses import dataclass
+import enum
+
+class OpenbisHierarcy(enum.Enum):
+    OBJECT = "OBJECT"
+    COLLECTION = "COLLECTION"
+    PROJECT = "PROJECT"
+    SPACE = "SPACE"
+    UNKNOWN = "UNKNOWN"
+    INSTANCE = "INSTANCE"
 
 
 class TreeElement(BaseModel):
+    """
+    A class to (recursively) represent a tree element
+    """
     id: str
+    code: str | None = None
+    permid: str | None = None
+    type: OpenbisHierarcy | None = None
     attributes: Dict[str, Any] | None = None
     children: List['TreeElement'] | List[None] = fields.Field([])
 
@@ -25,36 +42,111 @@ class TreeElement(BaseModel):
             self.children.append(el)
         else:
             pass
+    
+    def find(self, id: str) -> Optional['TreeElement']:
+        result = []
+        def inner_search(start: TreeElement, id: Str):
+            if start.id == id:
+                return start
+            else:
+                els =  [x for x in [inner_search(child, id) for child in start.children] if x is not None]
+                if len(els) > 0:
+                    return els[0]
+        return inner_search(self, id)
 
 
+@dataclass
+class OpenbisSampleInfo:
+    """
+    Class to hold the sample information 
+    from an openbis sample list
+    """
+    identifier: List[str] | None
+    code: str
+    permid: str
+    type: str
+    space: List[str] | None
+    project: List[str] | None
+    collection: List[str] | None
+
+
+def split_identifier(id: str, sep='/') -> List[str] | None:
+    return list(filter(None, id.split(sep)))
 
 def get_samples_with_info(ob: pybis.Openbis) -> List[List[str]]:
-    objs = ob.get_objects(attrs=['space', 'experiment', 'project', 'code', 'type'])
-    paths = [o.split('/') for o in objs.df.identifier.to_list()]
+    """
+    Get a list of all samples in an openbis instance,
+    returns the list of paths for all objects
+    """
+    objs = ob.get_objects(attrs=['space', 'experiment', 'project', 'code', 'type', "permId"])
+    paths = [
+        OpenbisSampleInfo(split_identifier(o.identifier), o.code, o.permId, o.type, split_identifier(o.space), split_identifier(o.project), split_identifier(o.experiment)) for o in objs.df.itertuples()
+    ]
     return paths
 
-
-def tree_builder(paths: List[List[str]]) -> TreeElement:
+def path_to_openbis_type(path: List[str]) -> OpenbisHierarcy:
     """
-    Given a list of lists of sample paths [/a/b/c,[/c/d/e] builds
+    Given the depth in the sample tree of a sample, return the corresponding
+    type
+    """
+    match path:
+        case str(x), str(y), str(z):
+            res = OpenbisHierarcy.SPACE
+        case str(x), str(y):
+            res = OpenbisHierarcy.PROJECT
+        case str(x), *rest if rest is None:
+            res = OpenbisHierarcy.OBJECT
+        case _:
+            res = OpenbisHierarcy.UNKNOWN
+    return res
+
+
+def tree_builder(paths: List[OpenbisSampleInfo]) -> TreeElement:
+    """
+    Given a list of lists of `OpenbisSampleInfo` builds
     a tree representation (using `pydantic.BaseModel` as a base class)
     of the tree
     """
     def recurse_setdefault(array: List[str] | str, res: TreeElement):
-
+        print(array)
+        path_type = path_to_openbis_type(array)
         match array:
             #Leaf
-            case str(x) if x != '':
-                res.push(TreeElement(id=x, children=[]))
+            case str(x):
+                if x in res.children_id():
+                    return
+                else:
+                    res.push(TreeElement(id=x, children=[], type=path_type))
             case str(x), *rest:
                 if x not in res.children_ids():
-                    res.push(TreeElement(id=x, children=[]))
+                    res.push(TreeElement(id=x, children=[],  type=path_type))
                 recurse_setdefault(rest, res.get(x))
-    tree = TreeElement(id='', children = [])
+    tree = TreeElement(id='/', children = [], type=OpenbisHierarcy.INSTANCE)
     for p in paths:
-        recurse_setdefault(p, tree)
+        recurse_setdefault(p.identifier, tree)
     return tree
 
 
+def build_sample_tree_from_list(ob: pybis.Openbis) -> TreeElement:
+    sample_info = get_samples_with_info(ob)
+    base_tree = TreeElement(id='/', code='/', type=OpenbisHierarcy.INSTANCE)
+    #Add spaces
+    for space in ob.get_spaces().df.itertuples():
+        base_tree.push(TreeElement(id=space.code, code=space.code, permid=space.code, type=OpenbisHierarcy.SPACE))
+    #Add projects
+    for proj in ob.get_projects():
+        base_tree.get(id=proj.space.code).push(TreeElement(id=proj.identifier, code=proj.code, permid=proj.permId, type=OpenbisHierarcy.PROJECT))
+    #Add collections
+    for coll in ob.get_collections(attrs=['project', 'space', 'code']).df.itertuples():
+        base_tree.get(coll.space).get(coll.project).push(TreeElement(id=coll.identifier, code=coll.code, permid=coll.permId, type=OpenbisHierarcy.COLLECTION))
+    #Add objects
+    for samp in ob.get_objects(attrs=['project', 'space', 'code', 'experiment']).df.itertuples():
+        proj = base_tree.find(samp.project)
+        if proj:
+            proj.push(TreeElement(id=samp.identifier, code=samp.code, permid=samp.permId, type=OpenbisHierarcy.OBJECT))
+    return base_tree
+
 def build_sample_tree(ob: pybis.Openbis) -> TreeElement:
-    return tree_builder(get_samples_with_info(ob))
+    sample_info = get_samples_with_info(ob)
+    return tree_builder(sample_info)
+
