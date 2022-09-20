0c8d5955aa7039df16782a7f9d3d494a98ae94ef
Simone Baffelli <simone.baffelli@empa.ch>
2022-09-20 11:36:27 +0200

    Added parameter to configure uvicorn logging.

--- services/data-discovery/app/main.py
+++ services/data-discovery/app/main.py
@@ -8,9 +8,11 @@ from datastore.routers import login
 
 from datastore.app import create_app
 
-def serve(app: FastAPI, host:str = '0.0.0.0', port: int = 8080):
+import pathlib as pl
+
+def serve(app: FastAPI, host:str = '0.0.0.0', port: int = 8080, log_config: str = './uvicorn_log.yml'):
     """Serve the API using uvicorn."""
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host=host, port=port, log_config=log_config)
 
 """
 To configure the app, edit `.env` in the main directory
@@ -20,6 +22,7 @@ To configure the app, edit `.env` in the main directory
 parser = ap.ArgumentParser(description="Run data discovery backedn servrice")
 parser.add_argument("--host", type=str, help='Host to run the app on', default='0.0.0.0')
 parser.add_argument("--port", type=int, help='Port to run the app on', default=8080)
+parser.add_argument("--log-config", type=str, help='Port to run the app on', default='./uvicorn_log.yml')
 args = parser.parse_args()
 
 
@@ -29,6 +32,6 @@ app = create_app()
 
 
 #Serve the app
-serve(app, args.host, args.port)
+serve(app, args.host, args.port, args.log_config)
 
 
